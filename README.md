05c54050af7553c3a1fc5cbae8dae3395e943a5e
Michael Neale <michael.neale@gmail.com>
2012-11-07 17:18:43 +1100

    Finalising

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 #  Node.js clickstart
 
 This clickstart demonstrates a basic hello world node.js app on CloudBees with continuous deployment.
+Jenkins runs tests via npm. 
 
-<a href="https://grandcentral.cloudbees.com/?CB_clickstart=https://raw.github.com/michaelneale/nodejs-clickstart/master/clickstart.json"><img src="https://s3.amazonaws.com/cloudbees-downloads/clickstart/clickstart-now.png"/></a>
+package.json is used to specify what packages are needed - main.js is the default server app
+(you can set main_js via bees config:set to override it to something else)
+
+<a href="https://grandcentral.cloudbees.com/?CB_clickstart=https://raw.github.com/michaelneale/nodejs-clickstart/master/clickstart.json"><img src="https://d3ko533tu1ozfq.cloudfront.net/clickstart/deployInstantly.png"/></a>
+
+# To run manually locally
 
 
+0. Install node.js
+1. Clone this repo
+2. node app/main.js
+
+# To deploy manually to CloudBees
+
+1. cd app
+2. zip -r ../app.zip
+3. bees app:deploy -t node -RPLUGIN.SRC.nodejs=https://s3.amazonaws.com/cloudbees-downloads/clickstack/node-clickstack.zip ../app.zip
+
+
+# The node.js clickstack
+See here if you want to change how it work: https://github.com/CloudBees-community/node-clickstack
+The node-clickstack makes this possible - if you want to tweak how the node.js container works
+fork that repo, change it, and use it for the -RPLUGIN.SRC.nodejs url above.
+
+Otherwise - fork this ! 
 
 Copyright © 2012 Michael Neale
--- app/main.js
+++ app/main.js
@@ -3,8 +3,38 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.writeHead(200, {"Content-Type": "text/html"});
+
+  /** this is only used to show what to do next */
+  var popover = '<!-- this is used for first time display of clickstart - a template-->\
+    <div id="clickstart_content" style="display:none">\
+    <p>\
+      Congratulations, <a href="#CS_appManageUrl"><span>#CS_appName</span></a>, your <a href="#CS_docUrl"><span>#CS_name</span></a> application is now running.<br />\
+      To modify it, take the following steps to clone the source repository:\
+    </p>\
+    <div class="CB_codeSample">\
+      git clone #CS_source #CS_appName<br/>\
+          cd #CS_appName<br/>\
+          ---- do your magic edits ----<br/>\
+          git commit -m "This is now even better !"<br/>\
+          git push origin master\
+    </div>\
+    <p>Manage your application components at the following URLs:</p>\
+    <ul>\
+      <li><strong>Core application:</strong> <a href="#CS_appManageUrl">#CS_appManageUrl</a></li>\
+      <li><strong>Database:</strong> <a href="#CS_appManageUrl">#CS_appManageUrl</a></li>\
+      <li><strong>Jenkins Build System:</strong> <a href="#CS_appManageUrl">#CS_appManageUrl</a></li>\
+    </ul>\
+  </div>\
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js"></script>\
+  <script type="text/javascript" src="https://s3.amazonaws.com/cloudbees-downloads/clickstart/clickstart_intro.js"></script>\
+  <!-- end clickstart intro section -->';
+
+  response.end("<html><h1><code>Welcome to Node.js on CloudBees</code></h1>\
+    <code>Made with a <a href='https://github.com/CloudBees-community/nodejs-clickstart'>ClickStart</a></code><p>\
+    <code>How it <a href='https://github.com/CloudBees-community/node-clickstack'>works</a> (a ClickStack)\
+     and <a href='http://developer.CloudBees.com'>docs</a></code><p><p>\
+    " + popover + "</html>");
 });
 
 
